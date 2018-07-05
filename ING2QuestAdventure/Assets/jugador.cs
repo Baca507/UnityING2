a8a60c7abee48684067b227c89b0408a3cb7c6a1
Maez H. Aguilar V <maez-aguilar@hotmail.com>
2018-07-04 19:05:20 -0500

    Scripts
    son los cs

---
+++ ING2QuestAdventure/Assets/jugador.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jugador : MonoBehaviour {
+    public float speed;
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moverHorizontal = Input.GetAxis("Horizontal");
+        float moverVertical = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(moverHorizontal,moverVertical,0.0f);
+
+        rb.AddForce(movimiento * speed * Time.deltaTime);
+        
+    }
+}
---
+++ ING2QuestAdventure/Assets/jugador2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jugador2 : MonoBehaviour
+{
+    float a = 20f;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector2.up * a  * 10 * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector2.down * a * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector2.right * a * 10 * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector2.left * a  * Time.deltaTime);
+    }
+}
+//Es para mover y Rotar//﻿
